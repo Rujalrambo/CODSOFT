1c0d6f8b8f2971eb13365f0e50b83e86a7423978
Rujal <rujalshrsth@gmail.com>
2024-09-30 19:05:43 +0545

    Final commit

--- Calculator/static/js/index.js
+++ Calculator/static/js/index.js
@@ -15,30 +15,3 @@ function calculate() {
     display.value = "Error";
   }
 }
-
-//---------parser part--------
-/*
-let display = document.getElementById('display');
-
-function appendToDisplay(value) {
-    display.value += value;
-}
-
-function clearDisplay() {
-    display.value = '';
-}
-
-function calculate() {
-    try {
-        const result = parseExpression(display.value);
-        display.value = result;
-    } catch (error) {
-        display.value = 'Error';
-    }
-}
-
-function parseExpression(expression) {
-    const sanitizedExpression = expression.replace(/[^-()\d/*+.]/g, '');
-    return Function(`'use strict'; return (${sanitizedExpression})`)();
-}
-*/
